5c58ac5334738a6cf11d0518b78c642526c8ba78
Only Meeps Sub Account <finn@meep.land>
2025-12-05 07:00:05 -0800

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,33 @@
+async function downloadTxtFile() {
+    const filename = document.getElementById("FileName").value + ".html";
+    const CustomURL = "https://"+ document.getElementById("GameURL").value;
+    const TabName = document.getElementById("TabName").value;
+    try {
+        const response = await fetch("template.txt");
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        let templateContent = await response.text();
+        templateContent = templateContent
+            .replace('[GAMEURL]', CustomURL)
+            .replace('[TABNAME]', TabName)
+
+        const blob = new Blob([templateContent], { type: 'text/html' });
+
+        const downloadLink = document.createElement('a');
+        downloadLink.href = URL.createObjectURL(blob);
+        downloadLink.download = filename;
+
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+
+        document.body.removeChild(downloadLink);
+        URL.revokeObjectURL(downloadLink.href);
+
+        alert(`Custom file "${filename}" generated and downloaded!`);
+
+    } catch (error) {
+        console.error('Error processing template file:', error);
+        alert('Failed to load or process the template file.');
+    }
+}
